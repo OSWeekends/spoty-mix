36dffd83f7fcffdd2728f7389680fa305ed90bec
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-11-13 13:45:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {
     firebase: {
-        apiKey: "AIzaSyC8u7xVgemdWfNFBcjEssgFhaecdqIKCV0",
-        authDomain: "spoty-mix.firebaseapp.com",
-        databaseURL: "https://spoty-mix.firebaseio.com",
-        storageBucket: "spoty-mix.appspot.com",
-        messagingSenderId: "1040957135146"
+        apiKey: "",
+        authDomain: "xxx.firebaseapp.com",
+        databaseURL: "https://xxx.firebaseio.com",
+        storageBucket: "xxx.appspot.com",
+        messagingSenderId: "xxx"
     },
     spotify: {
-        clientID: "1f50800e0a584d4384243758e8db9ae6",
-        clientSecret: "d2fd5261dcc743a782e6713979b156d9",
+        clientID: "client_id",
+        clientSecret: "client_secret",
     }
 };
 
